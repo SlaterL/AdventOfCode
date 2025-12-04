6e6f38e876181bd58fcf5660eabf0e015fac7672
Slater Lloyd <slater.lloyd@vivint.com>
2025-12-03 23:32:54 -0700

    day 4 2025

---
+++ cmd/2025_04/main.go
@@ -0,0 +1,135 @@
+package main
+
+import (
+	"advent-of-code/internal/parse"
+	"flag"
+	"fmt"
+	"log"
+	"path/filepath"
+)
+
+func main() {
+	part := flag.String("part", "both", "Which part to run: 1, 2, or both")
+	inputFile := flag.String("input", "input.txt", "What test file should be used")
+	flag.Parse()
+	inputPath := filepath.Join("cmd", "2025_04", *inputFile)
+
+	lines, err := parse.ParseLines(inputPath)
+	if err != nil {
+		log.Fatalf("failed to parse input file: %v", err)
+	}
+
+	switch *part {
+	case "1":
+		fmt.Println("Part 1:", part1(lines))
+	case "2":
+		fmt.Println("Part 2:", part2(lines))
+	case "both":
+		fmt.Println("Part 1:", part1(lines))
+		fmt.Println("Part 2:", part2(lines))
+	default:
+		log.Fatalf("unknown part: %s (expected 1, 2, or both)", *part)
+	}
+}
+
+func part1(lines []string) any {
+	sum := 0
+	rowMax := len(lines[0]) - 1
+	colMax := len(lines) - 1
+	for row, line := range lines {
+		for col, char := range line {
+			if char != '@' {
+				continue
+			}
+			surrounding := 0
+
+			if row > 0 {
+				if col > 0 && lines[row-1][col-1] == '@' {
+					surrounding++
+				}
+				if lines[row-1][col] == '@' {
+					surrounding++
+				}
+				if col < colMax && lines[row-1][col+1] == '@' {
+					surrounding++
+				}
+			}
+			if col > 0 && lines[row][col-1] == '@' {
+				surrounding++
+			}
+			if col < colMax && lines[row][col+1] == '@' {
+				surrounding++
+			}
+			if row < rowMax {
+				if col > 0 && lines[row+1][col-1] == '@' {
+					surrounding++
+				}
+				if lines[row+1][col] == '@' {
+					surrounding++
+				}
+				if col < colMax && lines[row+1][col+1] == '@' {
+					surrounding++
+				}
+			}
+			if surrounding <= 3 {
+				sum++
+			}
+		}
+	}
+	return sum
+}
+
+func part2(lines []string) any {
+	sum := 0
+	rowMax := len(lines[0]) - 1
+	colMax := len(lines) - 1
+	for {
+		removed := 0
+		for row, line := range lines {
+			for col, char := range line {
+				if char != '@' {
+					continue
+				}
+				surrounding := 0
+
+				if row > 0 {
+					if col > 0 && lines[row-1][col-1] == '@' {
+						surrounding++
+					}
+					if lines[row-1][col] == '@' {
+						surrounding++
+					}
+					if col < colMax && lines[row-1][col+1] == '@' {
+						surrounding++
+					}
+				}
+				if col > 0 && lines[row][col-1] == '@' {
+					surrounding++
+				}
+				if col < colMax && lines[row][col+1] == '@' {
+					surrounding++
+				}
+				if row < rowMax {
+					if col > 0 && lines[row+1][col-1] == '@' {
+						surrounding++
+					}
+					if lines[row+1][col] == '@' {
+						surrounding++
+					}
+					if col < colMax && lines[row+1][col+1] == '@' {
+						surrounding++
+					}
+				}
+				if surrounding <= 3 {
+					sum++
+					removed++
+					lines[row] = lines[row][:col] + "." + lines[row][col+1:]
+				}
+			}
+		}
+		if removed == 0 {
+			break
+		}
+	}
+	return sum
+}
